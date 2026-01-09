c73afa757541deb9e341fe57b067b9aa97a31afb
William Zhao <williamzhao@Mac.attlocal.net>
2026-01-09 17:29:21 -0500

    added homepage

---
+++ tiger-front/src/components/ArticleDropdownMenu.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase/db";
+import { collection, getDocs } from "firebase/firestore";
+
+const ArticleDropdownMenu = ({ value, onSelect }) => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const data = await getDocs(collection(db, "articles"));
+      const docs = data.docs.map(doc => ({id: doc.id, ...doc.data()}))
+      setArticles(docs);
+    };
+
+    fetchArticles();
+  }, []);
+
+  const handleChange = (event) => {
+    onSelect?.(event.target.value)
+  };
+
+  return (
+    <label>
+      <select value={value ?? ""} onChange={handleChange} className="w-full border-2 border-gray-200 p-2 overflow-y-auto truncate">
+        <option value="">Select an article</option>
+        {articles.map(article => (<option key={article.id} value={article.id} className="truncate">{article.title}</option>))}
+      </select>
+    </label>
+  );
+};
+
+export default ArticleDropdownMenu;
---
+++ tiger-front/src/components/CardMedium.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { fetchArticle } from "../firebase/db";
+import ArticleDropdownMenu from "./ArticleDropdownMenu";
+
+const CardMedium = ({articleID, setArticle, removeArticle, className = ''}) => {
+    const [article, getArticle] = useState(null)
+
+    useEffect(() => {
+      if (!articleID) {
+        getArticle(null)
+        return
+      }
+      const loadArticle = async () => {
+        const data = await fetchArticle(articleID)
+        getArticle(data)
+      }
+
+      loadArticle()
+    }, [articleID])
+
+    if (!article) return (
+        <div className={`h-full bg-gray-50 border-2 border-gray-200 p-3 flex items-center justify-center ${className}`}>
+            <ArticleDropdownMenu value={articleID ?? ""} onSelect={setArticle}/>
+        </div>
+    )
+
+    else return (
+        <div className={` relative h-full border-gray-200 p-3 ${className}`}>
+            <button onClick={removeArticle} className = "absolute top-2 right-2 text-red-500 hover:text-red-700 font-bold">
+                ×
+            </button>
+            <div className = "flex items-end h-full">
+                <h1 className="text-xl font-semibold justify-end">{article.title}</h1>
+            </div>
+        </div>
+    )
+}
+export default CardMedium
