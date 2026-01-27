28090f31e990eb35f38715a9c319e620e9642308
William Zhao <williamzhao@dynamic-oit-ip4-wifirestricted03-10-48-39-3.princeton.edu>
2026-01-26 21:40:24 -0500

    Configured local environment

--- tiger-front/src/firebase/firebaseconfig.js
+++ tiger-front/src/firebase/firebaseconfig.js
@@ -1,18 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDX1aEckJeHypcOqLBEvEiwSuCcX2kAKEI",
-  authDomain: "the-princeton-tiger.firebaseapp.com",
-  projectId: "the-princeton-tiger",
-  storageBucket: "the-princeton-tiger.firebasestorage.app",
-  messagingSenderId: "97404712630",
-  appId: "1:97404712630:web:ec785cc4717022825d1817",
-  measurementId: "G-99C7WJV0HM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
