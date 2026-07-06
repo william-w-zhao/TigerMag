35d83d517925815b64f785470e6d90a8aad3680d
William Zhao <williamzhao@Mac.attlocal.net>
2026-07-06 12:11:26 -0400

    Minor page changes

--- tiger-front/src/components/Footer.jsx
+++ tiger-front/src/components/Footer.jsx
@@ -14,13 +14,12 @@ const Footer = () => {
                     <div className="flex items-center justify-center lg:justify-start gap-4">
                         <img src={TigerWhite} alt="Logo" onClick={() => navigate("/")} className="h-8 w-auto cursor-pointer"/>
                         <h2 className="px-4 text-base font-medium text-white hover:text-orange-400 transition-colors duration-300 text-center lg:text-left">
-                            Weather in Princeton: 83°F ☀️ Sunny
+                            Weather in Princeton: 84°F ☀️ Sunny
                         </h2>
                     </div>
                     <div className="hidden lg:flex items-center justify-end gap-4">
                         <button className={NAV_BTN} onClick={() => navigate("/news")}>News</button>
-                        <button className={NAV_BTN} onClick={() => navigate("/opinion")}>Opinion</button>
-                        <button className={NAV_BTN} onClick={() => navigate("/archive")}>Archive</button>
+                        <button className={NAV_BTN} onClick={() => navigate("/issues")}>Issues</button>
                     </div>
                 </div>
             </div>
